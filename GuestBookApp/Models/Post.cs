4295854e1c01a25fe0fa6cfbcc50e8114d63edd3
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-02 14:02:06 +0200

    Create Post model

---
+++ GuestBookApp/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GuestBookApp.Models
+{
+    public class Post : BaseEntity
+    {
+        public string Message { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
