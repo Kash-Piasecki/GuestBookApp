25c83cbc3e3a4cee6ea3b2098aa69da4a2f71693
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-02 14:10:31 +0200

    Create context for DB connection

---
+++ GuestBookApp/Data/GuestBookContext.cs
@@ -0,0 +1,15 @@
+﻿using GuestBookApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuestBookApp.Data
+{
+    public class GuestBookContext : DbContext
+    {
+        public GuestBookContext(DbContextOptions<GuestBookContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
