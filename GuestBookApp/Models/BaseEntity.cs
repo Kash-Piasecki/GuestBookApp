f0266ce2ab1fff768ef1c47912ed5415785ba054
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-02 13:59:53 +0200

    Create BaseEntity model

---
+++ GuestBookApp/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GuestBookApp.Models
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; init; }
+
+        protected BaseEntity() => Id = new Guid();
+    }
+}
