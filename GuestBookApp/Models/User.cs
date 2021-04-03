8875d225be8969705b52b14372f63a641654cc96
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-03 10:37:36 +0200

    Add Validation of the fields

--- GuestBookApp/Models/User.cs
+++ GuestBookApp/Models/User.cs
@@ -5,10 +5,13 @@ namespace GuestBookApp.Models
     public class User : BaseEntity
     {
         [Required]
+        [MinLength(3, ErrorMessage = "Minimum length is 3 characters.")]
+        [RegularExpression("^[a-z]*$", ErrorMessage = "Only english letters.")]
         public string Name { get; set; }
         
         [Required]
         [DataType(DataType.EmailAddress)]
+        [MinLength(4, ErrorMessage = "Minimum length is 4 characters.")]
         public string Email { get; set; }
     }
 }
