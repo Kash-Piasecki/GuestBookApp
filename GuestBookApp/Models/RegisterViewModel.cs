55c901d74165ecbab1ca90b9c6fe7d0fe93dd68b
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-12 14:13:43 +0200

    Create Register page

--- GuestBookApp/Models/RegisterViewModel.cs
+++ GuestBookApp/Models/RegisterViewModel.cs
@@ -7,15 +7,21 @@ namespace GuestBookApp.Models
     public class RegisterViewModel
     {
         [Required]
+        [MinLength(3, ErrorMessage = "Your username will be visiblle under every post. Minimum 3 characters, maximum 20.")]
+        public string Name { get; set; }
+        
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
+        
+        
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+        
         [DataType(DataType.Password)]
         [DisplayName("Confirm Password")]
         [Compare("Password", ErrorMessage = "Password and confirmation do not match.")]
         public string ConfirmPassword { get; set; }
-        
     }
 }
