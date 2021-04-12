a905b922d6ea1d931288949882e6e66281a1f383
Lukasz Piasecki <KWHQ38@motorolasolutions.com>
2021-04-12 14:46:18 +0200

    Allow user to register

--- GuestBookApp/Controllers/AccountController.cs
+++ GuestBookApp/Controllers/AccountController.cs
@@ -1,13 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using GuestBookApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace GuestBookApp.Controllers
 {
     public class AccountController : Controller
     {
-        
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // True for permanent cookie. False for session cookie
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Post");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
         }
     }
 }
